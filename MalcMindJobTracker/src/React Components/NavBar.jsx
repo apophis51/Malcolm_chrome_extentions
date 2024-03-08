6513b788c9520d6ff3e60508dc75527e99f25c7c
apophis51 <malcolmxvernon@hotmail.com>
2024-03-08 02:38:06 -0500

    update

---
+++ MalcMindJobTracker/src/React Components/NavBar.jsx
@@ -0,0 +1,54 @@
+
+
+export default function navBar() {
+
+
+    function colorDom(setUnset) {
+        var children = document.querySelectorAll('*');
+        if (setUnset == 'set') {
+            children.forEach(function (child) {
+                child.style.border = '2px solid red';
+            });
+        }
+        if (setUnset == 'unset') {
+            children.forEach(function (child) {
+                child.style.border = '';
+            });
+        }
+    }
+
+    function linkActivator(setUnset){
+        var links = document.querySelectorAll('a');
+        if (setUnset == 'set') {
+            links.forEach(function (link) {
+                link.style.pointerEvents = 'none';
+            });
+        }
+        if (setUnset == 'unset') {
+            links.forEach(function (link) {
+                link.style.pointerEvents = 'auto';
+            });
+        }
+    }
+
+    function removeButtons(){
+        let buttonContainer = document.querySelectorAll('.button-container');
+        buttonContainer.forEach(function(button) {
+            button.remove();
+          });
+    }
+
+    return (
+        <>
+            <div className="navbar bg-base-100 fixed z-10 gap-10 ">
+                <button className='btn' onClick={() => colorDom('set')}>Color DOM</button>
+                <button className='btn' onClick={() => colorDom('unset')}>UNColor DOM</button>
+                <button className='btn' onClick={() => linkActivator('set')}>Deactivate Links</button>
+                <button className='btn' onClick={() => linkActivator('unset')}>Reactivate Links</button>
+                <button className='btn' onClick={() => removeButtons()}>Remove All Injected Buttons</button>
+            </div>
+            <div className='pt-20'>
+            </div>
+        </>
+    )
+}
--- MalcMindJobTracker/src/main.jsx
+++ MalcMindJobTracker/src/main.jsx
@@ -1,106 +1,102 @@
 import React from 'react'
 // import ReactDOM from 'react-dom/client'
 import Buttons from './React Components/Buttons.jsx'
+import NavBar from './React Components/NavBar.jsx'
 // import './index.css'
- import  {createRoot} from 'react-dom/client'
-
-
-
-
-
+import { createRoot } from 'react-dom/client'
 import * as buttonUtils from './VanillaUtils/buttonUtils.js';
+
+
+let myButtons = [
+  "Job Title",
+  "Company",
+  "Description",
+  "Save",
+  "Remove Buttons",
+  "Color DOM",
+  "UNColor DOM",
+  "Deactivate Links",
+  "Reactivate Links",
+  "Remove All Injected Buttons"
+]
+
 
 // let exportData = {}
 
+let navDiv = document.createElement('div');
+let navInstance = createRoot(navDiv)
 
+let documentBody = document.querySelector('body')
+documentBody.prepend(navDiv)
+
+navInstance.render(
+  <React.StrictMode>
+    <NavBar />
+  </React.StrictMode>,
+)
 
 function handleClick(event) {
 
   let reactElement = document.createElement('div');
-  reactElement
-reactElement.className = 'React-Component';
-
-// ReactDOM.createRoot(reactElement).render(
-//   <React.StrictMode>
-//     <Buttons />
-//   </React.StrictMode>,
-// )
-let reactInstance = createRoot(reactElement)
+  reactElement.className = 'React-Component';
+  let reactInstance = createRoot(reactElement)
 
   // Get the target node that was clicked
   var clickedNode = event.target;
   clickedNode.style.backgroundColor = 'lightyellow';
+
+
+
+  
   let documentText = clickedNode.textContent
-// Create a container div for the buttons
-var buttonContainer = document.createElement('div');
-buttonContainer.className = 'button-container';
+  // Create a container div for the buttons
+  var buttonContainer = document.createElement('div');
+  buttonContainer.className = 'button-container';
 
-var buttonRemove = buttonUtils.removeButton(buttonContainer);
-buttonRemove.style.backgroundColor = 'lightgreen';
-var button1 = buttonUtils.createButton('Job_Title', clickedNode.textContent);
-button1.style.backgroundColor = 'lightblue';
-var button2 = buttonUtils.createButton('Company', clickedNode.textContent);
-var button3 = buttonUtils.createButton('Job_Description', clickedNode.textContent);
-let button4 = buttonUtils.saveButton()
+  var buttonRemove = buttonUtils.removeButton(buttonContainer);
+  buttonRemove.style.backgroundColor = 'lightgreen';
+  var button1 = buttonUtils.createButton('Job_Title', clickedNode.textContent);
+  button1.style.backgroundColor = 'lightblue';
+  var button2 = buttonUtils.createButton('Company', clickedNode.textContent);
+  var button3 = buttonUtils.createButton('Job_Description', clickedNode.textContent);
+  let button4 = buttonUtils.saveButton()
 
-// Append buttons to the container
-buttonContainer.appendChild(button1);
-buttonContainer.appendChild(button2);
-buttonContainer.appendChild(button3);
-buttonContainer.appendChild(button4);
-buttonContainer.appendChild(buttonRemove);
-buttonContainer.appendChild(reactElement)
-
-
-
-
-reactInstance.render(
-  <React.StrictMode>
-    <Buttons test={documentText}/>
-  </React.StrictMode>,
-)
+  // Append buttons to the container
+  buttonContainer.appendChild(button1);
+  buttonContainer.appendChild(button2);
+  buttonContainer.appendChild(button3);
+  buttonContainer.appendChild(button4);
+  buttonContainer.appendChild(buttonRemove);
+  buttonContainer.appendChild(reactElement)
 
 
 
 
+  reactInstance.render(
+    <React.StrictMode>
+      <Buttons test={documentText} />
+    </React.StrictMode>,
+  )
 
 
-// Insert the container above the clicked node
-console.log(clickedNode.textContent)
-if(
-  clickedNode.textContent != 'Job Title' &&  clickedNode.textContent != 'Company' && clickedNode.textContent != 'Description' && clickedNode.textContent != 'Save' && clickedNode.textContent != 'Remove Buttons'){
-clickedNode.parentNode.insertBefore(buttonContainer, clickedNode);
-}
 
 
-// Disable all links within the clicked node
-var links = clickedNode.querySelectorAll('a');
-links.forEach(function(link) {
-  link.style.pointerEvents = 'none';
-});
-
-// Reset the background color of other nodes (if any)
-// var allNodes = document.querySelectorAll('*');
-// allNodes.forEach(function(node) {
-//     if (node !== clickedNode) {
-//         node.style.backgroundColor = ''; // Reset to default or remove this line to keep the previous background
-//     }
-// });
 
 
-// Add a red border to all children of the clicked node
-var children = clickedNode.querySelectorAll('*');
-children.forEach(function(child) {
-   child.style.border = '2px solid red';
-});
+  // Insert the container above the clicked node
+  console.log(clickedNode.textContent)
 
-// Reset the background color of other nodes (if any)
-// var allNodes = document.querySelectorAll('*');
-// allNodes.forEach(function(node) {
-//     if (node !== clickedNode) {
-//         node.style.backgroundColor = ''; // Reset to default or remove this line to keep the previous background
-//     }
-// });
+  if (!myButtons.includes(clickedNode.textContent)) {
+
+    clickedNode.parentNode.insertBefore(buttonContainer, clickedNode);
+  }
+
+
+  // Disable all links within the clicked node
+  var links = clickedNode.querySelectorAll('a');
+  links.forEach(function (link) {
+    link.style.pointerEvents = 'none';
+  });
 
   // Reset the background color of other nodes (if any)
   // var allNodes = document.querySelectorAll('*');
@@ -108,6 +104,14 @@ children.forEach(function(child) {
   //     if (node !== clickedNode) {
   //         node.style.backgroundColor = ''; // Reset to default or remove this line to keep the previous background
   //     }
+  // });
+
+
+  // Add a red border to all children of the clicked node
+  var children = clickedNode.querySelectorAll('*');
+  children.forEach(function (child) {
+    child.style.border = '2px solid red';
+  });
 }
 
 // Add a click event listener to the document
